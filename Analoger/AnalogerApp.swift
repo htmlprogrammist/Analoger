011437eb9ba56b398d436174ec2ab50a93e3aff7
Egor Badmaev <badmaeve2511@gmail.com>
2021-06-29 21:29:31 +0300

    init: start of the project 'Analoger'

---
+++ Analoger/AnalogerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogerApp.swift
+//  Analoger
+//
+//  Created by Егор Бадмаев on 29.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnalogerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
